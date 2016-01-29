a5e86ed672ba15d3f96e3c4fd04ca81e30d76989
Jason Power <c00185326@itcarlow.ie>
2016-01-29 19:57:21 +0000

    Controller 1+2 Done : )

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -24,14 +24,16 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <string>
-
-
+#include "P1Controls.h"
+#include "P2Controls.h"
 
 sf::Time deltaTime;
 sf::Clock myClock;
 
 sf::Texture tex;
 sf::Sprite spr;
+
+sf::Text text1, text2;
 
 
 // Game Modes
@@ -43,9 +45,7 @@ byte gameMode = MAINMENU;
 
 void Init()
 {
-	// Initialise shtuff
-	// Check that assets are been loaded
-	tex.loadFromFile("resources/controls.png");
+
 }
 
 void LoadContent()
@@ -57,7 +57,8 @@ void LoadContent()
 
 void UpdateMainMenu()
 {
-
+		text1.setString(std::to_string(P1Controls::Instance().m_leftAnalogAngle));
+		text2.setString(std::to_string(P2Controls::Instance().m_leftAnalogAngle));
 }
 void UpdateGame()
 {
@@ -72,6 +73,9 @@ void Update()
 	deltaTime = myClock.getElapsedTime();
 	myClock.restart();
 
+
+	P1Controls::Instance().Update();
+	P2Controls::Instance().Update();
 	// Update Game Modes
 	/////////////////////////////
 	switch (gameMode)
@@ -90,7 +94,9 @@ void Update()
 
 void(DrawMainMenu(sf::RenderWindow &p_window))
 {
-
+	p_window.draw(spr);
+	p_window.draw(text1);
+	p_window.draw(text2);
 }
 void(DrawGame(sf::RenderWindow &p_window))
 {
@@ -129,6 +135,24 @@ int main()
 	// Create the main window 
 	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "SFML First Program");
 
+
+	//load a font
+	sf::Font font;
+	font.loadFromFile("C:\\Windows\\Fonts\\GARA.TTF");
+
+	////create a formatted text string
+	text1.setFont(font);
+	text1.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
+	text1.setPosition(20, 40);
+	text1.setCharacterSize(20);
+
+	text2.setFont(font);
+	text2.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
+	text2.setPosition(200, 40);
+	text2.setCharacterSize(20);
+
+
+
 	// Initialize
 	/////////////////////////////
 	Init();
@@ -159,10 +183,6 @@ int main()
 		/////////////////////////////
 		Draw(window);
 
-
-
-		// Finally, display rendered frame on screen 
-		window.display();
 	} //loop back for next frame
 
 	return EXIT_SUCCESS;
---
+++ P2Controls.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "SFML\System\Vector2.hpp"
+#include "SFML\System\Time.hpp"
+#include <vector>
+class P2Controls
+{
+public:
+
+	const int m_DEADZONEA, m_DEADZONEB;
+	bool m_leftStickEnabled;
+	bool m_rightStickEnabled;
+	bool m_rightBumperPressed, m_leftBumperPressed, m_anyKeyPressed;
+	sf::Vector2f m_leftAnalogStick, m_rightAnalogStick, m_leftAnalogStickPrev;
+	float m_leftAnalogAngle, m_rightAnalogAngle;
+	void Update();
+	sf::Vector2f GetLeftStickAxis();
+	sf::Vector2f GetRightStickAxis();
+	static P2Controls& Instance();
+	int m_counter;
+	int m_buttonCount;
+	std::vector<bool>m_buttons;
+	std::vector<bool>m_buttonsPrev;
+	bool m_isControllerConnected;
+
+	bool CheckIfControllerIsConnected();
+
+private:
+	P2Controls();
+};
+
+
